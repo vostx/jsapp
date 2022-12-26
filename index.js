f0adfedf520163810ab33f91bcef00c564376d69
vostx <86882820+vostx@users.noreply.github.com>
2022-12-26 15:55:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Я зробив цей проект і можу піти грати в геншин')
+  response.send('Тестова перевірка')
 }) 
 
 app.listen(app.get('port'), function() {
