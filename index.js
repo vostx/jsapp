e4d0967ec88b973e9c0528cec0746c6c502f17be
vostx <86882820+vostx@users.noreply.github.com>
2023-04-02 15:09:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('test')
+  response.send('Дипломна робота Стогній В. 401-ТК')
 }) 
 
 app.listen(app.get('port'), function() {
